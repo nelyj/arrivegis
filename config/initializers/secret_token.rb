e3125c7317e1895d863c92d0cd062016f4b02222
Nelson Pato Jiménez <developer.jimenez@gmail.com>
2013-11-02 20:46:32 -0300

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arrivegis::Application.config.secret_token = '4477072899cc2aa12d06bb9086259d9117ac5570cf3bec51a966738004610255e98e7df155d2f489ba7c86a9f675264ffddc0de8b5bfe626e9ee7a5b11090716'
