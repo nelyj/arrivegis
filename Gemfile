6439cbe04039ea4bee1081c45161b8739aaa6919
Nelson Pato Jiménez <developer.jimenez@gmail.com>
2013-11-03 01:27:34 -0300

    Se agrega menu y se agrega devise gem para la autentificación

--- Gemfile
+++ Gemfile
@@ -24,7 +24,8 @@ gem 'jquery-rails'
 gem 'haml'
 gem 'haml-rails'
 gem 'simple_form'
-
+gem 'devise'
+gem 'html2haml'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type
+    when :alert
+      "alert alert-danger"
+    when :notice
+      "alert alert-success"
+    else
+      ""
+    end  
+  end
 end
