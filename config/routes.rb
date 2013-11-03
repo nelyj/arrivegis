a6b6ba90010671212d8309241200b38aa80eaa05
Nelson Pato Jiménez <developer.jimenez@gmail.com>
2013-11-03 01:42:48 -0300

    Se agrega el modelo de usuario para la autentificación

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Arrivegis::Application.routes.draw do
+  root :to => 'home#index'
   devise_for :users
 
-  root :to => 'home#index'
+  devise_scope :user do
+    get 'registrar', to: 'devise/registrations#new', as: :registrar
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
+  
 end
